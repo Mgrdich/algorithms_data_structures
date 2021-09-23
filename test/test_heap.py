5182956c6b7080215437a82a46bf03b96936f617
Mgrdich <mgotm13@gmail.com>
2021-09-23 17:15:24 +0400

    adding testing modules classes

---
+++ test/test_heap.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestHeap(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/test_stack.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestStack(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
